29794a5576932ea2a4a2f2cda7e946234cca3813
Francisco Contreras <fcocc77@gmail.com>
2024-10-02 18:05:36 -0300

    udpate

--- __init__.py
+++ __init__.py
@@ -2,11 +2,14 @@ import os
 import nuke  # type: ignore
 
 build = os.path.join(os.path.dirname(__file__), 'build')
-nuke.pluginAddPath(build)
+plugins = '{}/Nuke{}.{}'.format(build,
+                                nuke.NUKE_VERSION_MAJOR, nuke.NUKE_VERSION_MINOR)
 
-menu = nuke.menu('Nodes').addMenu('Examples')
+if os.path.isdir(plugins):
+    nuke.pluginAddPath(plugins)
+    menu = nuke.menu('Nodes').addMenu('Examples')
 
-for p in os.listdir(build):
-    name = p.split('.')[0]
-    nuke.load(name)
-    menu.addCommand(name)
+    for p in os.listdir(plugins):
+        name = p.split('.')[0]
+        nuke.load(name)
+        menu.addCommand(name)
--- compile.sh
+++ compile.sh
@@ -1,10 +1,13 @@
 source /opt/rh/gcc-toolset-9/enable
 
-nuke_include='/opt/Nuke14.0v7/include'
-mkdir build
+nuke_version=14.0
+revision=7
+nuke_include="/opt/Nuke$nuke_version""v$revision/include"
+
+build="build/Nuke$nuke_version"
+mkdir -p $build
 
 for cpp in $(ls plugins); do
     so="${cpp%.cpp}.so"
-    gcc -shared -fPIC -I$nuke_include -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0 -o build/$so plugins/$cpp
+    gcc -shared -fPIC -I$nuke_include -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=0 -o $build/$so plugins/$cpp
 done
-
